7dbeaa052916ab0a0c6a4ee66699fbac750a8a86
Victor Brown <sirbrv@gmail.com>
2023-04-04 20:20:15 -0300

    db sequelizen

--- src/configDB/configDB.js
+++ src/configDB/configDB.js
@@ -1,18 +1,30 @@
 const dbConfig = require("./database.js");
 const { Sequelize, DataTypes } = require("sequelize");
-
+/*
+DB_HOST: us-cdbr-east-06.cleardb.net
+DB_DATABASE: heroku_3a90d0af39b56c6
+DB_USER: b66ab2862cf7c6
+DB_PASSWORD: 684ccc08
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  port: "3306",
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  },
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-});
+*/
+const sequelize = new Sequelize(
+  "heroku_b72450ee00ac91b",
+  "b954e91af793b9",
+  "b0b4f80e",
+  {
+    host: "us-cdbr-east-06.cleardb.net",
+    dialect: dbConfig.dialect,
+    port: "3306",
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle,
+    },
+    socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+  }
+);
 
 sequelize
   .authenticate()
