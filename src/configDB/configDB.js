362e47d8dabecb21dd011b26ec511c767740bbd0
Victor Brown <sirbrv@gmail.com>
2023-04-05 08:55:44 -0300

    actualización bd

--- src/configDB/configDB.js
+++ src/configDB/configDB.js
@@ -5,36 +5,26 @@ const DB_HOST = process.env.DB_HOST; //"us-cdbr-east-06.cleardb.net";
 const DB_DATABASE = process.env.DB_DATABASE; //"heroku_3a90d0af39b56c6";
 const DB_USER = process.env.DB_USER; //"b66ab2862cf7c6";
 const DB_PASSWORD = process.env.DB_PASSWORD; //"684ccc08";
+
 if (dbConfig.port === 4000) {
-  const sequelize = new Sequelize(
-    dbConfig.database,
-    dbConfig.user,
-    dbConfig.password,
-    {
-      host: dbConfig.host,
-      dialect: dbConfig.dialect,
-      port: "3306",
-      pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle,
-      },
-    }
-  );
-} else {
-  const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
-    host: DB_HOST,
-    dialect: dbConfig.dialect,
-    port: "3306",
-    pool: {
-      max: dbConfig.pool.max,
-      min: dbConfig.pool.min,
-      acquire: dbConfig.pool.acquire,
-      idle: dbConfig.pool.idle,
-    },
-  });
+  DB_DATABASE = dbConfig.database;
+  DB_USER = dbConfig.user;
+  DB_PASSWORD = dbConfig.password;
+  DB_HOST = dbConfig.host;
 }
+
+const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: dbConfig.dialect,
+  port: "3306",
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
+});
+
 sequelize
   .authenticate()
   .then(() => {
